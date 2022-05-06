d63e0b53dc4736b64690858e350cd56efa54eb3f
wd-uni-git2022 <104198435+wd-uni-git2022@users.noreply.github.com>
2022-05-06 11:30:36 +0200

    added staff page

---
+++ js/staff.js
@@ -0,0 +1,33 @@
+students = [
+    {
+        firstName: "joe", lastName: "doe", courses: ["design", "programming1"]
+    },
+    {
+        firstName: "johanna", lastName: "doe", courses: ["math", "programming1"]
+    }
+]
+
+
+function getStudents(){
+    return students;
+}
+
+function courseInfo(name) {
+
+    document.getElementById("studentList").hidden = false;
+
+    let studentTable = document.getElementById("student-table-body");
+
+    studentTable.innerHTML = "";
+
+    let students = getStudents();
+
+    for (let student of students) {
+        if(student.courses.includes(name)){
+            let studentRow = studentTable.insertRow();
+            studentRow.insertCell(0).innerText = student.firstName;
+            studentRow.insertCell(1).innerText = student.lastName;
+        }
+
+    }
+}
