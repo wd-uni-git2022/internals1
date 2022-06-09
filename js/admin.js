905fa23126e63fd098685dfd8410fa2281b8d8f6
wd-uni-git2022 <104198435+wd-uni-git2022@users.noreply.github.com>
2022-06-09 22:21:49 +0200

    added department and semester dropdown selection

--- js/admin.js
+++ js/admin.js
@@ -6,7 +6,8 @@ let studentList = [
         dob: "01.01.1990",
         gender: "male",
         department: "Art",
-        email_id: "johndoe@example.org"
+        email_id: "johndoe@example.org",
+        joiningDate: "2016-10-01"
     },
     {
         studentId: 1,
@@ -15,7 +16,8 @@ let studentList = [
         dob: "01.01.1980",
         gender: "female",
         department: "Computer Science",
-        email_id: "josephinedoe@example.org"
+        email_id: "josephinedoe@example.org",
+        joiningDate: "2016-07-01"
     },
     {
         studentId: 2,
@@ -24,7 +26,8 @@ let studentList = [
         dob: "01.01.1985",
         gender: "other",
         department: "Computer Science",
-        email_id: "jodoe@example.org"
+        email_id: "jodoe@example.org",
+        joiningDate: "2016-07-01"
     },
     {
         studentId: 3,
@@ -33,7 +36,8 @@ let studentList = [
         dob: "01.01.2000",
         gender: "male",
         department: "Computer Science",
-        email_id: "johandoe@example.org"
+        email_id: "johandoe@example.org",
+        joiningDate: "2016-10-01"
     },
     {
         studentId: 4,
@@ -42,7 +46,8 @@ let studentList = [
         dob: "01.01.1995",
         gender: "female",
         department: "Computer Science",
-        email_id: "johanadoe@example.org"
+        email_id: "johanadoe@example.org",
+        joiningDate: "2016-07-01"
     }
 ];
 let staffList = [
@@ -55,7 +60,24 @@ let staffList = [
         emailId: "josi@staff.example.org"
     }
 ];
+let departments = [
+    "Art",
+    "Computer Science"
+];
+let semesters = {
+    Winter: {
+        startMonth: 10,
+        endMonth: 2
+    },
+    Summer: {
+        startMonth: 4,
+        endMonth: 9
+    }
+};
 
+
+const DEFAULT_DEPARTMENT_SELECTION = "Choose department";
+const DEFAULT_SEMESTER_SELECTION = "Choose semester";
 
 function getStudents() {
     return studentList;
@@ -65,13 +87,13 @@ function getStaff() {
     return staffList;
 }
 
-function loadStudentList() {
+function loadStudentList(students) {
     let studentTable = document.getElementById("student-table-body");
 
     //document.getElementById("student-table-body").innerHTML = "";
     studentTable.innerHTML = "";
 
-    let students = getStudents();
+    //let students = getStudents();
 
     for (let student of students) {
         let studentRow = studentTable.insertRow();
@@ -82,10 +104,12 @@ function loadStudentList() {
         studentRow.insertCell(4).innerText = student.gender;
         studentRow.insertCell(5).innerText = student.department;
         studentRow.insertCell(6).innerText = student.email_id;
+        studentRow.insertCell(7).innerText = student.joiningDate;
 
     }
 
 }
+
 
 function loadStaffList() {
     let staffTable = document.getElementById("staff-table-body");
@@ -110,15 +134,16 @@ function loadStaffList() {
 let maxDOB;
 let minDOB;
 let minJoiningDate;
-function setup(){
+
+function setup() {
     // dob validation
     let dobDatepicker = document.getElementById("add-student-dob");
 
     maxDOB = new Date();
-    maxDOB.setFullYear(maxDOB.getFullYear()-17);
+    maxDOB.setFullYear(maxDOB.getFullYear() - 17);
 
     minDOB = new Date();
-    minDOB.setFullYear(minDOB.getFullYear()-60);
+    minDOB.setFullYear(minDOB.getFullYear() - 60);
 
     dobDatepicker.max = maxDOB.toISOString().split("T")[0];
     dobDatepicker.min = minDOB.toISOString().split("T")[0];
@@ -129,10 +154,72 @@ function setup(){
     minJoiningDate.setFullYear(2015);
     joiningDateDatepicker.min = minJoiningDate.toISOString().split("T")[0];
 
+    // dropdown department
+    let dropdownDepartment = document.getElementById("department");
+    let defaultOption = document.createElement("option");
+    defaultOption.text = DEFAULT_DEPARTMENT_SELECTION;
+    dropdownDepartment.add(defaultOption)
+    departments.forEach(department => {
+        let option = document.createElement("option");
+        option.value = department;
+        option.text = department;
+        dropdownDepartment.add(option);
+    });
+
+    // dropdown semester
+    let dropdownSemester = document.getElementById("semester");
+    let defaultOptionSemester = document.createElement("option");
+    defaultOptionSemester.text = DEFAULT_SEMESTER_SELECTION;
+    dropdownSemester.add(defaultOptionSemester)
+    Object.keys(semesters).forEach(semester =>{
+        let option = document.createElement("option");
+        option.value = semester;
+        option.text = semester;
+        dropdownSemester.add(option);
+    });
+
+
+}
+
+function filterByDepartment(students) {
+    let value = document.getElementById("department").value;
+    console.log("selected department: " + value);
+    if (value === DEFAULT_DEPARTMENT_SELECTION) {
+        return students;
+    } else {
+        return students.filter(student => student.department === value);
+    }
+}
+
+
+function filterBySemester(students) {
+    let value = document.getElementById("semester").value;
+    console.log("selected semester: " + value);
+    if (value === DEFAULT_SEMESTER_SELECTION) {
+        return students;
+    } else {
+        let semesterStart = semesters[value].startMonth;
+        let semesterEnd = semesters[value].endMonth;
+
+
+        return students.filter(student => {
+            let joiningDateMonth = student.joiningDate.split("-")[1]; // or getMonth()
+            console.log(joiningDateMonth)
+            return joiningDateMonth >= semesterStart && joiningDateMonth <= semesterEnd;
+        });
+    }
+}
+
+function applyFilters() {
+    // All filters get applied to the student data
+    let students = getStudents();
+    students = filterByDepartment(students);
+    students = filterBySemester(students);
+    loadStudentList(students);
 }
 
 setup();
-loadStudentList();
+loadStudentList(getStudents());
 loadStaffList();
 
 function studentOptionsClick(buttonId) {
@@ -191,22 +278,21 @@ function addStudent() {
         return;
     }
 
-    if(studentData.dob < maxDOB || studentData.dob > minDOB){
+    if (studentData.dob < maxDOB || studentData.dob > minDOB) {
         console.error("dob error");
         alert("dob error");
         return;
     }
 
-    if(studentData.joiningDate < minJoiningDate){
+    if (studentData.joiningDate < minJoiningDate) {
         console.error("joining date error");
         alert("joining date error");
         return;
     }
 
 
-
     studentList.push(studentData);
-    loadStudentList();
+    loadStudentList(getStudents());
 
 }
 
@@ -219,20 +305,21 @@ function deleteStudent() {
 }
 
 
-function addStaff(){
+function addStaff() {
     console.log("add staff");
 }
 
 const TYPE_STUDENT = "student";
 const TYPE_STAFF = "staff";
+
 function showInfo(type) {
     let studentInfo = document.getElementById("student-info");
     let staffInfo = document.getElementById("staff-info");
 
-    if(type === TYPE_STUDENT){
+    if (type === TYPE_STUDENT) {
         staffInfo.hidden = true;
         studentInfo.hidden = false;
-    }else if(type === TYPE_STAFF){
+    } else if (type === TYPE_STAFF) {
         studentInfo.hidden = true;
         staffInfo.hidden = false;
     }
